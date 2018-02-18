f4309cd8453af23956f5ec58418a558c0c5eeb10
Steven Haws <stevenjhaws@gmail.com>
2018-02-17 20:29:24 -0700

    got the array and references added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 var tries = 0;
-var timerVar = setInterval(countTimer, 1000);
+var timerVar
 var totalSeconds = 0;
+var card;
+var cardNum
+var answers = [
+  "Dog",
+  "Cat",
+  "Bird",
+  "Elephant",
+  "Whale",
+  "Shark",
+  "Bear",
+  "Lion",
+  "Horse",
+  "Cow",
+  "Dog",
+  "Cat",
+  "Bird",
+  "Elephant",
+  "Whale",
+  "Shark",
+  "Bear",
+  "Lion",
+  "Horse",
+  "Cow"
+];
 
 function clickTry(){
-  tries += 0.5;
-  document.getElementById("attempts").innerHTML =  Math.ceil(tries);
+  tries += 1;
+  document.getElementById("attempts").innerHTML =  tries;
 };
 
 function clearGame(){
@@ -15,35 +39,48 @@ function clearGame(){
 
 function countTimer() {
    ++totalSeconds;
-   var hour = Math.floor(totalSeconds /3600);
-   var minute = Math.floor((totalSeconds - hour*3600)/60);
-   var seconds = totalSeconds - (hour*3600 + minute*60);
-
-   document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+  //  var hour = Math.floor(totalSeconds /3600);
+  //  var minute = Math.floor((totalSeconds - hour*3600)/60);
+   var seconds = totalSeconds //- (hour*3600 + minute*60);
+   document.getElementById("timer").innerHTML = seconds;
+  //  document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
 }
 
 function newGame(){
   clearGame();
+  //add array randomizer
+  var timerVar = setInterval(countTimer, 1000);
+};
+
+function matchClick(){
+  cardNum = document.getElementById(this.id).innerHTML;
+  document.getElementById(this.id).classList.remove("btn-primary");
+  document.getElementById(this.id).classList.add("btn-info");
+  document.getElementById(this.id).innerHTML = answers[cardNum - 1]
+  document.getElementById(this.id).element.disabled = true;
+
+  // //if 
+  // clickTry();
 };
 
 document.getElementById("new-game").addEventListener("click", newGame);
-document.getElementById("btn1").addEventListener("click", clickTry);
-document.getElementById("btn2").addEventListener("click", clickTry);
-document.getElementById("btn3").addEventListener("click", clickTry);
-document.getElementById("btn4").addEventListener("click", clickTry);
-document.getElementById("btn5").addEventListener("click", clickTry);
-document.getElementById("btn6").addEventListener("click", clickTry);
-document.getElementById("btn7").addEventListener("click", clickTry);
-document.getElementById("btn8").addEventListener("click", clickTry);
-document.getElementById("btn9").addEventListener("click", clickTry);
-document.getElementById("btn10").addEventListener("click", clickTry);
-document.getElementById("btn11").addEventListener("click", clickTry);
-document.getElementById("btn12").addEventListener("click", clickTry);
-document.getElementById("btn13").addEventListener("click", clickTry);
-document.getElementById("btn14").addEventListener("click", clickTry);
-document.getElementById("btn15").addEventListener("click", clickTry);
-document.getElementById("btn16").addEventListener("click", clickTry);
-document.getElementById("btn17").addEventListener("click", clickTry);
-document.getElementById("btn18").addEventListener("click", clickTry);
-document.getElementById("btn19").addEventListener("click", clickTry);
-document.getElementById("btn20").addEventListener("click", clickTry);
+document.getElementById("btn1").addEventListener("click", matchClick);
+document.getElementById("btn2").addEventListener("click", matchClick);
+document.getElementById("btn3").addEventListener("click", matchClick);
+document.getElementById("btn4").addEventListener("click", matchClick);
+document.getElementById("btn5").addEventListener("click", matchClick);
+document.getElementById("btn6").addEventListener("click", matchClick);
+document.getElementById("btn7").addEventListener("click", matchClick);
+document.getElementById("btn8").addEventListener("click", matchClick);
+document.getElementById("btn9").addEventListener("click", matchClick);
+document.getElementById("btn10").addEventListener("click", matchClick);
+document.getElementById("btn11").addEventListener("click", matchClick);
+document.getElementById("btn12").addEventListener("click", matchClick);
+document.getElementById("btn13").addEventListener("click", matchClick);
+document.getElementById("btn14").addEventListener("click", matchClick);
+document.getElementById("btn15").addEventListener("click", matchClick);
+document.getElementById("btn16").addEventListener("click", matchClick);
+document.getElementById("btn17").addEventListener("click", matchClick);
+document.getElementById("btn18").addEventListener("click", matchClick);
+document.getElementById("btn19").addEventListener("click", matchClick);
+document.getElementById("btn20").addEventListener("click", matchClick);
