dec34771e4a19764b4d1ba386ac16ac8b6a7d5af
Steven Haws <stevenjhaws@gmail.com>
2018-02-19 11:25:04 -0700

    main functionality finished

--- js/main.js
+++ js/main.js
@@ -31,7 +31,20 @@ var answers = [
   "Horse",
   "Cow"
 ];
-var modulatedAnswers = []
+var modulatedAnswers
+
+function shuffle(myArray) {
+  var ctr = myArray.length, temp, index;
+
+  while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = myArray[ctr];
+      myArray[ctr] = myArray[index];
+      myArray[index] = temp;
+  }
+  return myArray;
+}
 
 function clickTry(){
   tries += 1;
@@ -42,6 +55,109 @@ function clearGame(){
   tries = 0;
   document.getElementById("attempts").innerHTML = tries;
   totalSeconds = 0;
+  document.getElementById("timer").innerHTML = totalSeconds;
+  document.getElementById("btn1").innerHTML = 1;
+  document.getElementById("btn2").innerHTML = 2;
+  document.getElementById("btn3").innerHTML = 3;
+  document.getElementById("btn4").innerHTML = 4;
+  document.getElementById("btn5").innerHTML = 5;
+  document.getElementById("btn6").innerHTML = 6;
+  document.getElementById("btn7").innerHTML = 7;
+  document.getElementById("btn8").innerHTML = 8;
+  document.getElementById("btn9").innerHTML = 9;
+  document.getElementById("btn10").innerHTML = 10;
+  document.getElementById("btn11").innerHTML = 11;
+  document.getElementById("btn12").innerHTML = 12;
+  document.getElementById("btn13").innerHTML = 13;
+  document.getElementById("btn14").innerHTML = 14;
+  document.getElementById("btn15").innerHTML = 15;
+  document.getElementById("btn16").innerHTML = 16;
+  document.getElementById("btn17").innerHTML = 17;
+  document.getElementById("btn18").innerHTML = 18;
+  document.getElementById("btn19").innerHTML = 19;
+  document.getElementById("btn20").innerHTML = 20;
+  document.getElementById("btn1").classList.remove("btn-primary");
+  document.getElementById("btn2").classList.remove("btn-primary");
+  document.getElementById("btn3").classList.remove("btn-primary");
+  document.getElementById("btn4").classList.remove("btn-primary");
+  document.getElementById("btn5").classList.remove("btn-primary");
+  document.getElementById("btn6").classList.remove("btn-primary");
+  document.getElementById("btn7").classList.remove("btn-primary");
+  document.getElementById("btn8").classList.remove("btn-primary");
+  document.getElementById("btn9").classList.remove("btn-primary");
+  document.getElementById("btn10").classList.remove("btn-primary");
+  document.getElementById("btn11").classList.remove("btn-primary");
+  document.getElementById("btn12").classList.remove("btn-primary");
+  document.getElementById("btn13").classList.remove("btn-primary");
+  document.getElementById("btn14").classList.remove("btn-primary");
+  document.getElementById("btn15").classList.remove("btn-primary");
+  document.getElementById("btn16").classList.remove("btn-primary");
+  document.getElementById("btn17").classList.remove("btn-primary");
+  document.getElementById("btn18").classList.remove("btn-primary");
+  document.getElementById("btn19").classList.remove("btn-primary");
+  document.getElementById("btn20").classList.remove("btn-primary");
+  document.getElementById("btn1").classList.remove("btn-info");
+  document.getElementById("btn2").classList.remove("btn-info");
+  document.getElementById("btn3").classList.remove("btn-info");
+  document.getElementById("btn4").classList.remove("btn-info");
+  document.getElementById("btn5").classList.remove("btn-info");
+  document.getElementById("btn6").classList.remove("btn-info");
+  document.getElementById("btn7").classList.remove("btn-info");
+  document.getElementById("btn8").classList.remove("btn-info");
+  document.getElementById("btn9").classList.remove("btn-info");
+  document.getElementById("btn10").classList.remove("btn-info");
+  document.getElementById("btn11").classList.remove("btn-info");
+  document.getElementById("btn12").classList.remove("btn-info");
+  document.getElementById("btn13").classList.remove("btn-info");
+  document.getElementById("btn14").classList.remove("btn-info");
+  document.getElementById("btn15").classList.remove("btn-info");
+  document.getElementById("btn16").classList.remove("btn-info");
+  document.getElementById("btn17").classList.remove("btn-info");
+  document.getElementById("btn18").classList.remove("btn-info");
+  document.getElementById("btn19").classList.remove("btn-info");
+  document.getElementById("btn20").classList.remove("btn-info");
+  document.getElementById("btn1").classList.remove("btn-warning");
+  document.getElementById("btn2").classList.remove("btn-warning");
+  document.getElementById("btn3").classList.remove("btn-warning");
+  document.getElementById("btn4").classList.remove("btn-warning");
+  document.getElementById("btn5").classList.remove("btn-warning");
+  document.getElementById("btn6").classList.remove("btn-warning");
+  document.getElementById("btn7").classList.remove("btn-warning");
+  document.getElementById("btn8").classList.remove("btn-warning");
+  document.getElementById("btn9").classList.remove("btn-warning");
+  document.getElementById("btn10").classList.remove("btn-warning");
+  document.getElementById("btn11").classList.remove("btn-warning");
+  document.getElementById("btn12").classList.remove("btn-warning");
+  document.getElementById("btn13").classList.remove("btn-warning");
+  document.getElementById("btn14").classList.remove("btn-warning");
+  document.getElementById("btn15").classList.remove("btn-warning");
+  document.getElementById("btn16").classList.remove("btn-warning");
+  document.getElementById("btn17").classList.remove("btn-warning");
+  document.getElementById("btn18").classList.remove("btn-warning");
+  document.getElementById("btn19").classList.remove("btn-warning");
+  document.getElementById("btn20").classList.remove("btn-warning");
+  document.getElementById("btn1").classList.add("btn-primary");
+  document.getElementById("btn2").classList.add("btn-primary");
+  document.getElementById("btn3").classList.add("btn-primary");
+  document.getElementById("btn4").classList.add("btn-primary");
+  document.getElementById("btn5").classList.add("btn-primary");
+  document.getElementById("btn6").classList.add("btn-primary");
+  document.getElementById("btn7").classList.add("btn-primary");
+  document.getElementById("btn8").classList.add("btn-primary");
+  document.getElementById("btn9").classList.add("btn-primary");
+  document.getElementById("btn10").classList.add("btn-primary");
+  document.getElementById("btn11").classList.add("btn-primary");
+  document.getElementById("btn12").classList.add("btn-primary");
+  document.getElementById("btn13").classList.add("btn-primary");
+  document.getElementById("btn14").classList.add("btn-primary");
+  document.getElementById("btn15").classList.add("btn-primary");
+  document.getElementById("btn16").classList.add("btn-primary");
+  document.getElementById("btn17").classList.add("btn-primary");
+  document.getElementById("btn18").classList.add("btn-primary");
+  document.getElementById("btn19").classList.add("btn-primary");
+  document.getElementById("btn20").classList.add("btn-primary");
+  modulatedAnswers = shuffle(answers);
+  buttonUnDisableAll();
 };
 
 function countTimer() {
@@ -54,8 +170,7 @@ function countTimer() {
 
 function newGame(){
   clearGame();
-  // modulatedAnswers = shuffle(answers)
-  //add array randomizer
+  document.getElementById("game-board").style = "display: block";
   var timerVar = setInterval(countTimer, 1000);
 };
 
@@ -113,14 +228,16 @@ function matchClick(){
     document.getElementById(firstId).classList.add("btn-warning");
     document.getElementById(firstId).innerHTML = answers[firstCardNum - 1]
     firstAnswer = document.getElementById(firstId).innerHTML
+    document.getElementById(firstId).disabled = true;
   }
-  else {
+  else if(firstCardNum != null && secondCardNum === null){
     secondId = this.id
     secondCardNum = document.getElementById(secondId).innerHTML;
     document.getElementById(secondId).classList.remove("btn-primary");
     document.getElementById(secondId).classList.add("btn-warning");
     document.getElementById(secondId).innerHTML = answers[secondCardNum - 1]
     secondAnswer = document.getElementById(secondId).innerHTML
+    document.getElementById(secondId).disabled = true;
     // buttonDisableAll();
     
     if (firstAnswer != secondAnswer){
@@ -130,7 +247,10 @@ function matchClick(){
       document.getElementById(firstId).innerHTML = firstCardNum
       document.getElementById(secondId).classList.remove("btn-warning");
       document.getElementById(secondId).classList.add("btn-primary");
-      document.getElementById(secondId).innerHTML = secondCardNum
+      document.getElementById(secondId).innerHTML = secondCardNum;
+      // buttonUnDisableAll();
+      document.getElementById(firstId).disabled = false;
+      document.getElementById(secondId).disabled = false;
       clickTry();
       firstChoice = null;
       secondChoice = null;
@@ -143,8 +263,9 @@ function matchClick(){
       document.getElementById(firstId).classList.add("btn-info");
       document.getElementById(secondId).classList.remove("btn-warning");
       document.getElementById(secondId).classList.add("btn-info");
-      //disable winning pair
+      // buttonUnDisableAll()
       clickTry();
+      setTimeout(function(){
       firstChoice = null;
       secondChoice = null;
       firstCardNum = null;
@@ -153,9 +274,12 @@ function matchClick(){
       if (rightAnswers === 10){
         alert("You Win!!")
       }
-
+     }, 1000)
     }
 
+  }
+  else {
+    alert("You can only select two options at a time. Please try again.")
   }
 };
 
@@ -180,3 +304,4 @@ document.getElementById("btn17").addEventListener("click", matchClick);
 document.getElementById("btn18").addEventListener("click", matchClick);
 document.getElementById("btn19").addEventListener("click", matchClick);
 document.getElementById("btn20").addEventListener("click", matchClick);
+buttonDisableAll()
